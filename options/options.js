7d898d4a3cd7cb2c74c0147549d827ecdc173ddb
Crowdbotics <team@crowdbotics.com>
2021-08-15 14:47:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_cloud_29824Identifier",
+  url: "https://rapid_cloud_29824Identifier.botics.co",
+  api: "https://rapid_cloud_29824Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
